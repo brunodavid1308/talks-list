5ef3367546c5f4b04b64218c29193999d133d2bc
bruno ortega <bruno_o_t@hotmail.com>
2022-10-13 11:54:46 +0200

    feat: add base project

---
+++ event-list/jest.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+  transformIgnorePatterns: ["node_modules/(?!nanoid)"],
+};
