155a7756fbdcc85b04e5efcf5359347c59f99f54
bruno ortega <bruno_o_t@hotmail.com>
2022-10-14 10:08:36 +0200

    styles: format code

--- event-list/src/features/domain/Event/interface/EventRepository.interface.ts
+++ event-list/src/features/domain/Event/interface/EventRepository.interface.ts
@@ -1,6 +1,6 @@
-import {Event} from "../Event"
+import { Event } from "../Event"
 
-export abstract class EventRepository{
+export abstract class EventRepository {
     public abstract findAll(): Promise<Event[]>;
     public abstract findByUuid(uuid: string): Promise<Event>;
 }
