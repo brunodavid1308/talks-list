350f9a9312e077dfc1fdac98ea26a92fbe6fe566
bruno ortega <bruno_o_t@hotmail.com>
2022-10-14 10:02:02 +0200

    feat: add jest configuration

--- event-list/jest.config.cjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "jsdom",
-  moduleNameMapper: {
-    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
-  },
-  transformIgnorePatterns: ["node_modules/(?!nanoid)"],
-};
